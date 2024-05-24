7c84f3f111ecc59c609a171baee9cb6b0e4c3468
PapaPaint <piercril000@byui.edu>
2024-05-23 20:51:07 -0600

    Fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString()); // 1/1
+        Console.WriteLine(fraction1.GetDecimalValue()); // 1
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString()); // 5/1
+        Console.WriteLine(fraction2.GetDecimalValue()); // 5
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString()); // 3/4
+        Console.WriteLine(fraction3.GetDecimalValue()); // 0.75
+
+        Fraction fraction4 = new Fraction();
+        fraction4.SetNumerator(1);
+        fraction4.SetDenominator(3);
+        Console.WriteLine(fraction4.GetFractionString()); // 1/3
+        Console.WriteLine(fraction4.GetDecimalValue()); // 0.3333333333333333
     }
 }
